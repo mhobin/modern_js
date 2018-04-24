a35da5bab7e1b186f6275abb1a1cc804f1e22e82
“MARGARET <hobin.margaret@gmailL.com>
2018-04-24 18:01:36 -0500

    install npm packages

---
+++ index.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+var math = require('mathjs');
+
+console.log('testing');
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+math.sqrt(-4); // 2i
+console.log(math.sqrt(-4));
